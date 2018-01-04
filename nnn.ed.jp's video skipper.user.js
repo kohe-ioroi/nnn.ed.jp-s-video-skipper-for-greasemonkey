1874bfc9bd369354b2a208795874142102c5097d
Kohe_Ioroi <ioroikouhei64@gmail.com>
2018-01-04 23:25:43 +0900

    最適化

--- nnn.ed.jp's video skipper.user.js
+++ nnn.ed.jp's video skipper.user.js
@@ -7,15 +7,11 @@
 (function(){
     var resSelect = document.evaluate('//*[@id="nextMovie"]',document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
     if(!resSelect.snapshotLength){
-        console.log('次の動画が存在しません。レポート・テストをチェックします...');
         var resSelect2 = document.evaluate('//*[@id="nextTest"]',document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
         if(!resSelect2.snapshotLength){
-            console.log('レポート・テストが存在しません。処理をスキップします。');
         }else{
-            console.log('レポート・テストの存在を確認しました。自動スキップを開始します。');
             setInterval(function(){document.getElementById('nextTest').click();}, 1000);}
     }else{
-        console.log('次の動画の存在を確認しました。自動スキップを開始します。');
         setInterval(function(){document.getElementById('nextMovie').click();}, 1000);}
 }
 )();
