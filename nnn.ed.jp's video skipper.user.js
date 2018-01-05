a579e8385a9832b5521c7b541382eab893f683a0
Kohe_Ioroi <ioroikouhei64@gmail.com>
2018-01-05 22:48:35 +0900

    バージョン0.1へアップデート
    動画スキップ終了時に自動的にアラートを発生

--- nnn.ed.jp's video skipper.user.js
+++ nnn.ed.jp's video skipper.user.js
@@ -2,13 +2,15 @@
 // @name           nnn.ed.jp's video skipper
 // @namespace      Kohe_Ioroi
 // @description    Skip Videos(beta)
+// @version        0.1
 // @include        https://secure.nnn.ed.jp/mypage/report/pc/movie/*
+// @include        https://secure.nnn.ed.jp/mypage/report/pc/test/*
 // ==/UserScript==
 (function(){
     var resSelect = document.evaluate('//*[@id="nextMovie"]',document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
     if(!resSelect.snapshotLength){
         var resSelect2 = document.evaluate('//*[@id="nextTest"]',document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
-        if(!resSelect2.snapshotLength){
+        if(!resSelect2.snapshotLength){alert("レポート・設問の回答を行ってください。");
         }else{
             setInterval(function(){document.getElementById('nextTest').click();}, 1000);}
     }else{
